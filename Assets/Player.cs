c18ef0f380f423fbf7ce7d8cf6fffd71f1ba3706
as <>
2021-02-06 05:10:35 +0900

    アセットのインポート

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1f;
+
+    public float rotateSpeed = 100f;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(0, 0, v);
+
+        velocity = transform.TransformDirection(velocity);
+
+        transform.localPosition += velocity * speed * Time.fixedDeltaTime;
+
+        transform.Rotate(0, h * rotateSpeed * Time.fixedDeltaTime, 0);
+
+        if (v > 0)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else if (v < 0)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
+}
